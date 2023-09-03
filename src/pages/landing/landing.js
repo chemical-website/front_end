f0f773850028100108e82644cf3e8d1f0603239d
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-09-03 12:14:33 +0330

    add structure

---
+++ src/pages/landing/landing.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Footer from "../../layout/footer/footer";
+
+const LandingPage = () => {
+    return ( 
+        <Fragment>
+            <Footer />
+        </Fragment>
+     );
+}
+ 
+export default LandingPage;
