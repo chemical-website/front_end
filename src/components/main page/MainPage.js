68e4cb62d262b6efbd4802d67c1a7dac7df272ba
At1X <amirmahdijafarian@gmail.com>
2023-10-22 01:23:41 +0330

    refactor and delete useless spaces

--- src/components/main page/MainPage.js
+++ src/components/main page/MainPage.js
@@ -8,8 +8,14 @@ import { AiFillCaretUp } from "react-icons/ai";
 import { FaShapes } from "react-icons/fa";
 import { BiSearch } from "react-icons/bi";
 import { IoSearchCircleSharp } from "react-icons/io5";
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText } from "@mui/material";
-import styles from "../cart/cart.style.module.css"
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
+import styles from "../cart/cart.style.module.css";
 import { MdOutlineOpenInNew } from "react-icons/md";
 import Order from "../../pages/product/order";
 
@@ -17,7 +23,7 @@ function MainPage() {
   // let [openimahsolat, setopenopenimahsolat] = useState(0);
   // let [opensanat, setopenopensanat] = useState(0);
   // let [openiconmahsol, setopenopeniconmahsol] = useState(0);
-  const [open , setOpen] = useState(0)
+  const [open, setOpen] = useState(0);
   const [openModal, setOpenModal] = useState(true);
   const [openModal2, setOpenModal2] = useState(false);
   const [openModal3, setOpenModal3] = useState(false);
@@ -51,83 +57,107 @@ function MainPage() {
     },
   ];
 
-  useEffect(()=>{
-    if (localStorage.getItem("token") !== ""){
-      setOpenModal(false)
+  useEffect(() => {
+    if (localStorage.getItem("token") !== "") {
+      setOpenModal(false);
+    } else {
+      setOpenModal(true);
     }
-    else{
-     setOpenModal(true)
-    }
-  }, [])
+  }, []);
 
   return (
     <>
       <div className={mainpage.BBox}>
         <div className={mainpage.TopBox}>
-          <UperPart/>
-          </div>
+          <UperPart />
+        </div>
         <div className={mainpage.DownBox}>
-          <DownerPart/>
+          <DownerPart />
         </div>
         <div className="flex items-center w-full justify-center mt-12 mb-7">
           <div className="w-5/6">
-            <button onClick={handleClickOpen2} className="bg-blue-600 text-white p-3 rounded-lg">کمک میخوای</button>
+            <button
+              onClick={handleClickOpen2}
+              className="bg-blue-600 text-white p-3 rounded-lg"
+            >
+              کمک میخوای
+            </button>
           </div>
-          </div>
+        </div>
         <Dialog
-        open={openModal}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-        maxWidth="md"
-      >
-        {/* <DialogTitle>{"Use Google's location service?"}</DialogTitle> */}
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            <div className="flex flex-col md:flex-row items-center" style={{height:"25rem"}}>
-                <div className="w-full md:w-full flex flex-col items-center" >
-                هر کشوری قانون و مقررات خاصی دارد که افراد باید تابع آن باشند. لازم به ذکر است که در صورت سرپیچی از قوانین، مجازاتی خواهند داشت؛ حال افراد باید بتوانند با آگاهی داشتن از قوانین کشور خود زندگی خوبی را پشت سر بگذارند.
-
-در حقیقت قوانین همان باید و نبایدهایی هستند که افراد باید بر اساس آن‌ها پیش روند. اگر جامعه و کشوری قانون و مقرراتی نداشته باشد، به هیچ ‌وجه پیشرفت نمی‌کند و همچنین به اهداف خود نخواهد رسید. 
+          open={openModal}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
+          maxWidth="md"
+        >
+          {/* <DialogTitle>{"Use Google's location service?"}</DialogTitle> */}
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+              <div
+                className="flex flex-col md:flex-row items-center"
+                style={{ height: "25rem" }}
+              >
+                <div className="w-full md:w-full flex flex-col items-center">
+                  هر کشوری قانون و مقررات خاصی دارد که افراد باید تابع آن باشند.
+                  لازم به ذکر است که در صورت سرپیچی از قوانین، مجازاتی خواهند
+                  داشت؛ حال افراد باید بتوانند با آگاهی داشتن از قوانین کشور خود
+                  زندگی خوبی را پشت سر بگذارند. در حقیقت قوانین همان باید و
+                  نبایدهایی هستند که افراد باید بر اساس آن‌ها پیش روند. اگر
+                  جامعه و کشوری قانون و مقرراتی نداشته باشد، به هیچ ‌وجه پیشرفت
+                  نمی‌کند و همچنین به اهداف خود نخواهد رسید.
                 </div>
+              </div>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <div className={styles.LinkSBox}>
+              <Link className="px-6 " to={"/app/s"}>
+                "میپذیرم"
+              </Link>
             </div>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-        <div className={styles.LinkSBox}><Link className="px-6 " to={"/app/s"}>"میپذیرم"</Link></div>
-        </DialogActions>
-      </Dialog>
+          </DialogActions>
+        </Dialog>
 
-      <Dialog
-        open={openModal2}
-        keepMounted
-        onClose={handleClose2}
-        aria-describedby="alert-dialog-slide-description"
-        maxWidth="md"
-      >
-        {/* <DialogTitle>{"Use Google's location service?"}</DialogTitle> */}
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            <div className="flex flex-col md:flex-row items-center" style={{height:"25rem"}}>
-                <div className="w-full md:w-full flex flex-col items-center" >
-                هر کشوری قانون و مقررات خاصی دارد که افراد باید تابع آن باشند. لازم به ذکر است که در صورت سرپیچی از قوانین، مجازاتی خواهند داشت؛ حال افراد باید بتوانند با آگاهی داشتن از قوانین کشور خود زندگی خوبی را پشت سر بگذارند.
-
-در حقیقت قوانین همان باید و نبایدهایی هستند که افراد باید بر اساس آن‌ها پیش روند. اگر جامعه و کشوری قانون و مقرراتی نداشته باشد، به هیچ ‌وجه پیشرفت نمی‌کند و همچنین به اهداف خود نخواهد رسید. 
+        <Dialog
+          open={openModal2}
+          keepMounted
+          onClose={handleClose2}
+          aria-describedby="alert-dialog-slide-description"
+          maxWidth="md"
+        >
+          {/* <DialogTitle>{"Use Google's location service?"}</DialogTitle> */}
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+              <div
+                className="flex flex-col md:flex-row items-center"
+                style={{ height: "25rem" }}
+              >
+                <div className="w-full md:w-full flex flex-col items-center">
+                  هر کشوری قانون و مقررات خاصی دارد که افراد باید تابع آن باشند.
+                  لازم به ذکر است که در صورت سرپیچی از قوانین، مجازاتی خواهند
+                  داشت؛ حال افراد باید بتوانند با آگاهی داشتن از قوانین کشور خود
+                  زندگی خوبی را پشت سر بگذارند. در حقیقت قوانین همان باید و
+                  نبایدهایی هستند که افراد باید بر اساس آن‌ها پیش روند. اگر
+                  جامعه و کشوری قانون و مقرراتی نداشته باشد، به هیچ ‌وجه پیشرفت
+                  نمی‌کند و همچنین به اهداف خود نخواهد رسید.
                 </div>
-            </div>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-              <button onClick={handleClickOpen3}> ثبت درخواست</button>
-        </DialogActions>
-      </Dialog>
-      <Dialog    open={openModal3}
-        keepMounted
-        onClose={handleClose3}
-        aria-describedby="alert-dialog-slide-description"
-        maxWidth="md">
-        <Order func={handleClose3} />
-      </Dialog>
+              </div>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <button onClick={handleClickOpen3}> ثبت درخواست</button>
+          </DialogActions>
+        </Dialog>
+        <Dialog
+          open={openModal3}
+          keepMounted
+          onClose={handleClose3}
+          aria-describedby="alert-dialog-slide-description"
+          maxWidth="md"
+        >
+          <Order func={handleClose3} />
+        </Dialog>
       </div>
     </>
   );
--- src/components/main page/UperPart.js
+++ src/components/main page/UperPart.js
@@ -5,51 +5,42 @@ import { AiFillCaretDown } from "react-icons/ai";
 import { AiFillCaretUp } from "react-icons/ai";
 import axios from "axios";
 import { BaseRoot } from "../../baseRoot";
-import ShapesIcon from '../../assets/Icons/Shapes.svg'
-import SettingsIcon from "../../assets/Icons/Settings.svg"
-import BuyCartIcon from "../../assets/Icons/BuyCart.svg"
-import SearchIcon from "../../assets/Icons/SearchIconWhite.svg"
-
-
+import ShapesIcon from "../../assets/Icons/Shapes.svg";
+import SettingsIcon from "../../assets/Icons/Settings.svg";
+import BuyCartIcon from "../../assets/Icons/BuyCart.svg";
+import SearchIcon from "../../assets/Icons/SearchIconWhite.svg";
 
 import { BsSearch } from "react-icons/bs";
-
-
-
 
 function MainPage() {
   let [openimahsolat, setopenopenimahsolat] = useState(0);
   let [opensanat, setopenopensanat] = useState(0);
   let [openiconmahsol, setopenopeniconmahsol] = useState(0);
-  const [mahsolatData , setMahsolatData] = useState([]);
-  const [masolat , setMahsolat ] = useState([]);
-  const [industry , setIndustry] = useState([])
+  const [mahsolatData, setMahsolatData] = useState([]);
+  const [masolat, setMahsolat] = useState([]);
+  const [industry, setIndustry] = useState([]);
   const config = {
-    headers:{
-      Authorization: localStorage.getItem("token")
-    }
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
   };
-  useEffect(()=>{
-    axios.get(`${BaseRoot}store/collections/` , config).then(
-      function(response){
-        setMahsolatData(response.data)
-      }
-    )
-  } , [openimahsolat])
-  useEffect(()=>{
-    axios.get(`${BaseRoot}store/products/` , config).then(
-      function(response){
-        setMahsolat(response.data)
-      }
-    )
-  } , [openiconmahsol])
-  useEffect(()=>{
-    axios.get(`${BaseRoot}store/industries/` , config).then(
-      function(response){
-        setIndustry(response.data)
-      }
-    )
-  } , [opensanat])
+  useEffect(() => {
+    axios
+      .get(`${BaseRoot}store/collections/`, config)
+      .then(function (response) {
+        setMahsolatData(response.data);
+      });
+  }, [openimahsolat]);
+  useEffect(() => {
+    axios.get(`${BaseRoot}store/products/`, config).then(function (response) {
+      setMahsolat(response.data);
+    });
+  }, [openiconmahsol]);
+  useEffect(() => {
+    axios.get(`${BaseRoot}store/industries/`, config).then(function (response) {
+      setIndustry(response.data);
+    });
+  }, [opensanat]);
   let data = [
     {
       name: "پلی  اتیلن ",
@@ -92,122 +83,123 @@ function MainPage() {
 
   return (
     <>
-     
-          <h1 className=" hidden md:block">شرکت تولید کننده پلیمر صنعتی تهران</h1>
+      <h1 className=" hidden md:block">شرکت تولید کننده پلیمر صنعتی تهران</h1>
 
-          <div className={mainpage.SearchBox}>
-            <button
-              className={mainpage.Chosdastebandi}
-              onClick={DastebandiSearch}
-            >
-              <i className={mainpage.leftIcon}>
-                {openimahsolat == 0 ? <AiFillCaretDown color="#27023b" /> : <AiFillCaretUp color="#27023b" />}
-              </i>
-              <span className={mainpage.SearchDastebandi}>
-               <span className="text-xl font-bold" style={{color: "#3B0359"}}> دسته بندی محصولات</span>
-                <i className={mainpage.rightIcon}>
-                  <img src={ShapesIcon} alt="ShapeIcon"/>
-                </i>
-              </span>
-            </button>
-            <button className={mainpage.Chosdastebandi} onClick={SanatSearch}>
-              <i className={mainpage.leftIcon}>
-                {opensanat == 0 ? <AiFillCaretDown color="#27023b" /> : <AiFillCaretUp color="#27023b" />}
-              </i>
-              <span className={mainpage.SearchDastebandi}>
-              <span className="text-xl font-bold" style={{color: "#3B0359"}}>انتخاب صنعت</span>
-                <i className={mainpage.rightIcon}>
-                  <img src={SettingsIcon} alt="SettingIcon"/>
-                </i>
-              </span>
-            </button>
-            <button className={mainpage.Chosdastebandi} onClick={MahsolSearch}>
-              <i className={mainpage.leftIcon}>
-                {openiconmahsol == 0 ? <AiFillCaretDown color="#27023b" /> : <AiFillCaretUp color="#27023b" />}
-              </i>
-              <div className={mainpage.SearchDastebandi}>
-                <span className="text-xl font-bold" style={{color: "#3B0359"}}>انتخاب محصول</span>
-                <i className={mainpage.rightIcon}>
-                  <img src={BuyCartIcon} alt="BuyCartIcon"/>
-                </i>
-              </div>
-            </button>
-
-
-
-            <div
-              className={
-                openimahsolat == 0
-                  ? mainpage.displaynone
-                  : mainpage.SearchOpenBox1
-              }
-            >
-              {/* <input placeholder="تایپ کنید" className={mainpage.inputf} /> */}
-              {/* <i className={mainpage.searchIcon}>
-                <BiSearch />
-              </i> */}
-              {mahsolatData.map((x) => {
-                return (
-                  <>
-                    <Link to={`/app/collections/search/${x.title}`}> {x.title}</Link>
-                  </>
-                );
-              })}
-            </div>
-
-
-
-            <div
-              className={
-                opensanat == 0
-                  ? mainpage.displaynone
-                  : mainpage.SearchOpenBox2
-              }
-            >
-              {/* <input placeholder="تایپ کنید" className={mainpage.inputf} /> */}
-              {/* <i className={mainpage.searchIcon}>
-                <BiSearch />
-              </i> */}
-              {industry.map((x) => {
-                return (
-                  <>
-                    <Link to={`/app/industries/search/${x.title}`}> {x.title}</Link>
-                  </>
-                );
-              })}
-            </div>
-
-
-
-            <div
-              className={
-                openiconmahsol == 0
-                  ? mainpage.displaynone
-                  : mainpage.SearchOpenBox3
-              }
-            >
-              {/* <input placeholder="تایپ کنید" className={mainpage.inputf} /> */}
-              {/* <i className={mainpage.searchIcon}>
-                <BiSearch />
-              </i> */}
-              {masolat.map((x) => {
-                return (
-                  <>
-                    <Link to={`/app/product/${x.id}`}> {x.title}</Link>
-                  </>
-                );
-              })}
-            </div>
-
-
-
-
-
-              <i className={mainpage.searchdown}>
-                <img src={SearchIcon} className="scale-50" alt="SearchIcon"/>
-              </i>
+      <div className={mainpage.SearchBox}>
+        <button className={mainpage.Chosdastebandi} onClick={DastebandiSearch}>
+          <i className={mainpage.leftIcon}>
+            {openimahsolat == 0 ? (
+              <AiFillCaretDown color="#27023b" />
+            ) : (
+              <AiFillCaretUp color="#27023b" />
+            )}
+          </i>
+          <span className={mainpage.SearchDastebandi}>
+            <span className="text-xl font-bold" style={{ color: "#3B0359" }}>
+              {" "}
+              دسته بندی محصولات
+            </span>
+            <i className={mainpage.rightIcon}>
+              <img src={ShapesIcon} alt="ShapeIcon" />
+            </i>
+          </span>
+        </button>
+        <button className={mainpage.Chosdastebandi} onClick={SanatSearch}>
+          <i className={mainpage.leftIcon}>
+            {opensanat == 0 ? (
+              <AiFillCaretDown color="#27023b" />
+            ) : (
+              <AiFillCaretUp color="#27023b" />
+            )}
+          </i>
+          <span className={mainpage.SearchDastebandi}>
+            <span className="text-xl font-bold" style={{ color: "#3B0359" }}>
+              انتخاب صنعت
+            </span>
+            <i className={mainpage.rightIcon}>
+              <img src={SettingsIcon} alt="SettingIcon" />
+            </i>
+          </span>
+        </button>
+        <button className={mainpage.Chosdastebandi} onClick={MahsolSearch}>
+          <i className={mainpage.leftIcon}>
+            {openiconmahsol == 0 ? (
+              <AiFillCaretDown color="#27023b" />
+            ) : (
+              <AiFillCaretUp color="#27023b" />
+            )}
+          </i>
+          <div className={mainpage.SearchDastebandi}>
+            <span className="text-xl font-bold" style={{ color: "#3B0359" }}>
+              انتخاب محصول
+            </span>
+            <i className={mainpage.rightIcon}>
+              <img src={BuyCartIcon} alt="BuyCartIcon" />
+            </i>
           </div>
-       
+        </button>
+
+        <div
+          className={
+            openimahsolat == 0 ? mainpage.displaynone : mainpage.SearchOpenBox1
+          }
+        >
+          {/* <input placeholder="تایپ کنید" className={mainpage.inputf} /> */}
+          {/* <i className={mainpage.searchIcon}>
+                <BiSearch />
+              </i> */}
+          {mahsolatData.map((x) => {
+            return (
+              <>
+                <Link to={`/app/collections/search/${x.title}`}>
+                  {" "}
+                  {x.title}
+                </Link>
+              </>
+            );
+          })}
+        </div>
+
+        <div
+          className={
+            opensanat == 0 ? mainpage.displaynone : mainpage.SearchOpenBox2
+          }
+        >
+          {/* <input placeholder="تایپ کنید" className={mainpage.inputf} /> */}
+          {/* <i className={mainpage.searchIcon}>
+                <BiSearch />
+              </i> */}
+          {industry.map((x) => {
+            return (
+              <>
+                <Link to={`/app/industries/search/${x.title}`}> {x.title}</Link>
+              </>
+            );
+          })}
+        </div>
+
+        <div
+          className={
+            openiconmahsol == 0 ? mainpage.displaynone : mainpage.SearchOpenBox3
+          }
+        >
+          {/* <input placeholder="تایپ کنید" className={mainpage.inputf} /> */}
+          {/* <i className={mainpage.searchIcon}>
+                <BiSearch />
+              </i> */}
+          {masolat.map((x) => {
+            return (
+              <>
+                <Link to={`/app/product/${x.id}`}> {x.title}</Link>
+              </>
+            );
+          })}
+        </div>
+
+        <i className={mainpage.searchdown}>
+          <img src={SearchIcon} className="scale-50" alt="SearchIcon" />
+        </i>
+      </div>
     </>
   );
 }
