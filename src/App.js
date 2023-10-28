6361e42eba593c012537b67638547e77f8d510fc
At1X <amirmahdijafarian@gmail.com>
2023-10-28 11:47:40 +0330

    add animation to pop up

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ import PicSlider from "./pages/product/picSlider";
 import { useWindowSize } from "@uidotdev/usehooks";
 import { RxCross2 } from "react-icons/rx";
 import SeeMoreIcon from "./assets/Icons/SeeMore.svg";
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Error404() {
   return (
@@ -35,8 +36,8 @@ function App() {
     }
     return () => {
       document.body.style.overflow = "unset";
-    }
-  }, [isModalOpen])
+    };
+  }, [isModalOpen]);
 
   return (
     <>
@@ -59,93 +60,102 @@ function App() {
         <Route path="app/news" element={<NewsPage />} />
         <Route path="app/s" element={<SignIn />} />
       </Routes>
-      {isModalOpen && (
-        <div
-          className="w-screen h-screen fixed left-0 top-0 right-0 m-auto overflow-auto z-40 shadow-md myBackdrop"
-          onClick={() => {
-            closeModal();
-          }}
-        >
-          <div className="w-full h-full flex flex-col justify-center items-center">
-            <div
-              className="bg-slate-50 rounded-2xl shadow-lg px-5 relative z-50"
-              style={
-                width > 1024
-                  ? { height: height / 2, width: width / 2 }
-                  : width > 500
-                  ? {
-                      height: height - 100,
-                      width: width - width / 3,
-                    }
-                  : { height: height - 100 , width: width - 50 }
-              }
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            >
-              <div className="flex flex-col lg:flex-row">
-                <div
-                  className="w-1/2 max-lg:w-full relative"
-                  style={{ height: height / 2 }}
-                >
-                  <div className="flex flex-col h-full justify-center">
-                    <PicSlider images={modalData.images} />
+      <AnimatePresence>
+        {isModalOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.2 }}
+            exit={{ opacity: 0 }}
+            className="w-screen h-screen fixed left-0 top-0 right-0 m-auto overflow-auto z-40 shadow-md myBackdrop"
+            onClick={() => {
+              closeModal();
+            }}
+          >
+            <div className="w-full h-full flex flex-col justify-center items-center">
+              <div
+                className="bg-slate-50 rounded-2xl shadow-lg px-5 relative z-50"
+                style={
+                  width > 1024
+                    ? { height: height / 2, width: width / 2 }
+                    : width > 500
+                    ? {
+                        height: height - 100,
+                        width: width - width / 3,
+                      }
+                    : { height: height - 100, width: width - 50 }
+                }
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                <div className="flex flex-col lg:flex-row">
+                  <div
+                    className="w-1/2 max-lg:w-full relative"
+                    style={{ height: height / 2 }}
+                  >
+                    <div className="flex flex-col h-full justify-center">
+                      <PicSlider images={modalData.images} />
+                    </div>
                   </div>
-                </div>
-                <div className="w-1/2 max-lg:w-full overflow-auto flex flex-col justify-center items-start text-justify">
-                  <div className="flex flex-col gap-1 p-5 h-fit">
-                    <span className="text-2xl font-bold">
-                      {modalData.title}
-                    </span>
-                    <span className="text-lg">
-                      {modalData.short_description}
-                    </span>
-                    <span className="mt-4">
-                      {modalData.description.length > 160
-                        ? modalData.description.substring(0, 160) +
-                          " [برای ادامه کلیک کنید]"
-                        : modalData.description}
-                    </span>
-                    <div className="flex flex-row gap-2">
-                      {modalData.tags.map((e) => {
-                        return (
-                          <div
-                            style={{ borderColor: "#7606B2", color: "#7606B2" }}
-                            className="mt-3 py-1 px-3 rounded-lg text-base border-2 border-solid w-fit font-bold cursor-default"
-                          >
-                            {e.tag.title}
-                          </div>
-                        );
-                      })}
+                  <div className="w-1/2 max-lg:w-full overflow-auto flex flex-col justify-center items-start text-justify">
+                    <div className="flex flex-col gap-1 p-5 h-fit">
+                      <span className="text-2xl font-bold">
+                        {modalData.title}
+                      </span>
+                      <span className="text-lg">
+                        {modalData.short_description}
+                      </span>
+                      <span className="mt-4">
+                        {modalData.description.length > 160
+                          ? modalData.description.substring(0, 160) +
+                            " [برای ادامه کلیک کنید]"
+                          : modalData.description}
+                      </span>
+                      <div className="flex flex-row gap-2">
+                        {modalData.tags.map((e) => {
+                          return (
+                            <div
+                              style={{
+                                borderColor: "#7606B2",
+                                color: "#7606B2",
+                              }}
+                              className="mt-3 py-1 px-3 rounded-lg text-base border-2 border-solid w-fit font-bold cursor-default"
+                            >
+                              {e.tag.title}
+                            </div>
+                          );
+                        })}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div
-                className="absolute top-5 left-5 cursor-pointer"
-                onClick={() => {
-                  closeModal();
-                }}
-              >
-                <RxCross2 size={25} />
-              </div>
-              <div
-                className="w-fit flex flex-row justify-center items-center p-5 absolute -left-7 -bottom-7 cursor-pointer"
-                style={{ backgroundColor: "#3B0359", borderRadius: "50%" }}
-              >
-                <Link
-                  to={`/app/product/${modalData.id}`}
+                <div
+                  className="absolute top-5 left-5 cursor-pointer"
                   onClick={() => {
                     closeModal();
                   }}
                 >
-                  <img src={SeeMoreIcon} alt="seeMore" />
-                </Link>
+                  <RxCross2 size={25} />
+                </div>
+                <div
+                  className="w-fit flex flex-row justify-center items-center p-5 absolute -left-7 -bottom-7 cursor-pointer"
+                  style={{ backgroundColor: "#3B0359", borderRadius: "50%" }}
+                >
+                  <Link
+                    to={`/app/product/${modalData.id}`}
+                    onClick={() => {
+                      closeModal();
+                    }}
+                  >
+                    <img src={SeeMoreIcon} alt="seeMore" />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
