d31a9a6b269c82406b43dada78d3ff5aa497dd22
At1X <amirmahdijafarian@gmail.com>
2024-03-13 20:42:05 +0330

    fix header bug

--- src/layout/header/TasfiyeBox.js
+++ src/layout/header/TasfiyeBox.js
@@ -2,44 +2,29 @@ import { Link } from "react-router-dom";
 import { AiOutlineLeft } from "react-icons/ai";
 import navigationBar from "./navigationBar.module.css";
 
-function TasfiyeBox({ show: s, subsData, id }) {
-  if (subsData.length > 0) {
-  }
-
+function TasfiyeBox({ show: s, data, id }) {
   if (s === id) {
     return (
       <>
         <div className={navigationBar.SBox}>
-          {subsData.length > 0 &&
-            subsData.map((e) => {
-              return (
-                <div key={e.title} className="flex flex-col">
-                  <span className="text-xl font-bold cursor-default">
-                    {e.title}
-                  </span>
-                  <div className="flex flex-wrap flex-col w-auto h-60 gap-1">
-                    {e.products &&
-                      e.products.map((prodData) => {
-                        return (
-                          <div className="flex flex-col">
-                            <span
-                              key={prodData.title}
-                              className="text-base cursor-pointer"
-                            >
-                              <Link
-                                className=""
-                                to={`/app/product/${prodData.id}`}
-                              >
-                                {prodData.title}
-                              </Link>
-                            </span>
-                          </div>
-                        );
-                      })}
+          <span className="text-xl font-bold cursor-default">{data.title}</span>
+          <div className="flex flex-wrap flex-col w-auto h-60 gap-1">
+            {data.products &&
+              data.products.map((prodData) => {
+                return (
+                  <div className="flex flex-col">
+                    <span
+                      key={prodData.title}
+                      className="text-base cursor-pointer"
+                    >
+                      <Link className="" to={`/app/product/${prodData.id}`}>
+                        {prodData.title}
+                      </Link>
+                    </span>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
         </div>
       </>
     );
